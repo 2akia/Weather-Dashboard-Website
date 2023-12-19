b9eaac4d12b797f1fab2487eace02f0a0203c025
2akia <2akssultana@gmail.com>
2023-12-19 19:25:41 +0000

    added the submit and weather api/narrowed down the selected weather datalist

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,21 +31,56 @@ $(document).ready(function () {
     
     });
 
+    form.on("submit", function (event) {
+        event.preventDefault();
+        let cityName = $("#search-input").val().trim();
 
+        console.log("City Name:", cityName);
 
+        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&appid=" + APIKey;
 
+        fetch(queryURL)
+            .then(function (response) {
+                if (!response.ok) {
+                    throw new Error("Weather data not available. Please check the city name and try again.");
+                }
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(queryURL);
+                console.log(data);
 
+                let selectedData = [
+                    data.list[2],
+                    data.list[10],
+                    data.list[18],
+                    data.list[26],
+                    data.list[34],
 
+                ];
 
+                updateForecast(selectedData);
 
-
-
-
-
-
-
-
-
-
-
+            })
     
+    });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
